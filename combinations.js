fed3cc2dc01a018c76ed0cc82d4c3caec59f243f
Jitendra <Jitendrs@thoughtworks.com>
2017-10-06 15:29:37 +0530

    Using reduce instead of for loop

--- combinations.js
+++ combinations.js
@@ -1,14 +1,12 @@
 function getCombinations(numbers){
-	var combinations = [];
-	numbers.forEach(e => {
-		var clone = JSON.parse(JSON.stringify(combinations));
-		clone.map(each => {
-			each.push(e);
+	return numbers.reduce((pv, cv) => {
+		var clone = JSON.parse(JSON.stringify(pv)).map(each => {
+			return each.concat([cv]);
 		});
-		if(clone.length > 0) combinations = combinations.concat(clone);
-		combinations.push([e]);
-	});
-	return combinations;
+		if(clone.length > 0) pv = pv.concat(clone);
+		pv.push([cv]);
+		return pv;
+	}, []);
 }
 
-console.log(getCombinations(['a', 'b', 'c']));
+console.log(getCombinations([1,2,3,4,5,6,7,8,9]));
